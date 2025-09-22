35ae20484b69bbbdc28de353373bfa9ea08077cc
abobabo <se@millennium-institute.org>
2025-09-22 16:39:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -470,7 +470,11 @@ with col_left:
     
                 df_gpt = st.session_state.df_extracted.copy()
                 df_gpt["Számlaszám"] = df_gpt["Számlaszám"].astype(str)
-                df_gpt.drop('1', axis=1)
+                
+                try:
+                    df_gpt.drop('1', axis=1, inplace=True)
+                except:
+                    pass
     
                 # --- átnevezés suffix-szel ---
                 df_gpt = df_gpt.add_suffix("_ai")
